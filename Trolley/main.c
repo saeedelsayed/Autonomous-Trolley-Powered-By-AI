c5455269928bd96c670f1ec276b6d58e34fa35e9
saeed elsayed <saidelsayed922@gmail.com>
2024-01-08 19:42:52 +0200

    adding the watch dog timer code

--- Trolley/main.c
+++ Trolley/main.c
@@ -1,39 +1,63 @@
-#include "services.h"
+#include <stdint.h>
 
-float32 distanceBetweenUserAndTrolley = 0.0;
-uint8 direction = 0;
-uint16 angleOrSpeed = 0;
+#define RCC_AHB1ENR   (*(unsigned long*) (0x40023800+0x30))
+#define RCC_RC        (*(unsigned long*) (0x40023800))
+#define GPIOB_ODR     (*(unsigned long*) (0x40020400+0x14))
+#define GPIOB_MODER   (*(unsigned long*) (0x40020400))
 
-int main()
+#define IWDG_BASE   0x40003000
+
+// IWDG registers
+#define IWDG_KR_OFFSET      0x00
+#define IWDG_PR_OFFSET      0x04
+#define IWDG_RLR_OFFSET     0x08
+
+#define IWDG_KEY_ENABLE     0x5555
+#define IWDG_KEY_REFRESH    0xAAAA
+#define IWDG_KEY_START      0xCCCC
+
+void IWDG_Init(void);
+void IWDG_Refresh(void);
+
+int main(void)
 {
-  /* enable clock for STM32 */
-  *((uint32*)0x40023800) |= (1 << 0);
   
-  /*enable clock for PORT B */
-  *((uint32*)0x40023830) |= (1 << 1);
+  RCC_RC |= (1<<0);
+  RCC_AHB1ENR |= (1 << 1);
   
-  /*enable clock for PORT A */
-  *((uint32*)0x40023830) |= (1 << 0);
+  IWDG_Init();
   
-    
-  SERVICES_systemInit();
+  GPIOB_MODER |= (1 << (5 * 2));
+  GPIOB_ODR |= (1 << 5);
+  for(int i =0; i <1000000; i++);
+  GPIOB_ODR &= ~(1 << 5);
   
-  uint8 returnValue;
-  
-    
-  while(1){
-    SERVICES_updateTrolleyData();
-    SERVICES_updateUserData();
-    distanceBetweenUserAndTrolley = SERVICES_getDistance();
-    SERVICES_decideDirection(&angleOrSpeed, &direction);
-    returnValue = SERVICES_sendCommandToAvrSlave(direction, angleOrSpeed);
-    
-    if(returnValue == FAIL){
-      continue;
-    }
-    
+
+  while (1)
+  {
+    // Feed the watchdog timer to prevent reset
+    //IWDG_Refresh();
   }
-  
-  return 0;
-  
 }
+
+
+void IWDG_Init(void)
+{
+  // Enable write access to IWDG_PR, IWDG_RLR registers
+  *((volatile uint32_t *)(IWDG_BASE + IWDG_KR_OFFSET)) = IWDG_KEY_ENABLE;
+
+  // Set the prescaler and reload values for a 2-second timeout
+  *((volatile uint32_t *)(IWDG_BASE + IWDG_PR_OFFSET)) = 4;    // Prescaler: 64
+  *((volatile uint32_t *)(IWDG_BASE + IWDG_RLR_OFFSET)) = 1000; // Reload value for 2-second timeout
+
+  // Start the watchdog timer
+  *((volatile uint32_t *)(IWDG_BASE + IWDG_KR_OFFSET)) = IWDG_KEY_START;
+}
+
+void IWDG_Refresh(void)
+{
+  // Feed the watchdog timer
+  *((volatile uint32_t *)(IWDG_BASE + IWDG_KR_OFFSET)) = IWDG_KEY_REFRESH;
+}
+
+
